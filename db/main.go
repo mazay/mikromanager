4dd80a42334c45a0079f0a18175aac3785c2d45f
Yev Valeyev <z.mazay@gmail.com>
2022-10-13 00:52:26 +0200

    draft db package

---
+++ db/main.go
@@ -0,0 +1,63 @@
+package api
+
+import (
+	"log"
+
+	"github.com/ostafen/clover"
+)
+
+type DB struct {
+	Path string
+	api  *clover.DB
+}
+
+func (db *DB) Init() (bool, error) {
+	database, err := clover.Open(db.Path)
+	if err != nil {
+		return false, err
+	}
+
+	db.api = database
+
+	exists, _ := db.api.HasCollection("devices")
+	if !exists {
+		db.api.CreateCollection("devices")
+	}
+
+	return true, nil
+}
+
+func (db *DB) Close() error {
+	return db.api.Close()
+}
+
+func (db *DB) Insert(collection string, values map[string]interface{}) (string, error) {
+	doc := clover.NewDocument()
+	doc.SetAll(values)
+
+	return db.api.InsertOne(collection, doc)
+}
+
+func (db *DB) Update(collection string, key string, value string, updates map[string]interface{}) error {
+	exists, _ := db.api.Query(collection).Where(clover.Field(key).Eq(value)).Exists()
+	if exists {
+		return db.api.Query(collection).Where(clover.Field(key).Eq(value)).Update(updates)
+	} else {
+		updates[key] = value
+		_, err := db.Insert(collection, updates)
+		return err
+	}
+}
+
+func (db *DB) Print() {
+	query := db.api.Query("devices")
+	docs, _ := query.FindAll()
+
+	for _, doc := range docs {
+		log.Println(doc)
+	}
+}
+
+func (db *DB) Export(collection string, filename string) {
+	db.api.ExportCollection(collection, filename)
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,28 @@ module github.com/mazay/mikromanager
 
 go 1.19
 
-require gopkg.in/routeros.v2 v2.0.0-20190905230420-1bbf141cdd91
+require (
+	github.com/ostafen/clover v1.2.0
+	gopkg.in/routeros.v2 v2.0.0-20190905230420-1bbf141cdd91
+)
+
+require (
+	github.com/cespare/xxhash v1.1.0 // indirect
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/dgraph-io/badger/v3 v3.2103.2 // indirect
+	github.com/dgraph-io/ristretto v0.1.0 // indirect
+	github.com/dustin/go-humanize v1.0.0 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/glog v1.0.0 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
+	github.com/google/flatbuffers v2.0.6+incompatible // indirect
+	github.com/klauspost/compress v1.15.9 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/satori/go.uuid v1.2.0 // indirect
+	go.opencensus.io v0.23.0 // indirect
+	golang.org/x/net v0.0.0-20220728211354-c7608f3a8462 // indirect
+	golang.org/x/sys v0.0.0-20220728004956-3c1f35247d10 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
+)
