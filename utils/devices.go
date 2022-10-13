88657e86060da4bb397df6b4665082305dbbf0df
Yev Valeyev <z.mazay@gmail.com>
2022-10-14 00:34:32 +0200

    devices utils

---
+++ utils/devices.go
@@ -0,0 +1,42 @@
+package utils
+
+import (
+	"encoding/json"
+)
+
+type Device struct {
+	Id                   string `json:"_id"`
+	Address              string `json:"address"`
+	ArchitectureName     string `json:"architecture-name"`
+	BadBlocks            int64  `json:"bad-blocks"`
+	BoardName            string `json:"board-name"`
+	BuildTime            string `json:"build-time"`
+	CPU                  string `json:"cpu"`
+	CpuCount             int    `json:"cpu-count"`
+	CpuFrequency         int    `json:"cpu-frequency"`
+	CpuLoad              int    `json:"cpu-load"`
+	FactorySoftware      string `json:"factory-software"`
+	FreeHddSpace         int64  `json:"free-hdd-space"`
+	FreeMemory           int64  `json:"free-memory"`
+	Identity             string `json:"identity"`
+	Platform             string `json:"platform"`
+	TotalHddSpace        int64  `json:"total-hdd-space"`
+	TotalMemory          int64  `json:"total-memory"`
+	Uptime               string `json:"uptime"`
+	Version              string `json:"version"`
+	WriteSectSinceReboot int64  `json:"write-sect-since-reboot"`
+	WriteSectTotal       int64  `json:"write-sect-total"`
+}
+
+func (d *Device) FromCloverDocs(docs []map[string]string) []*Device {
+	var deviceList []*Device
+
+	for _, doc := range docs {
+		dm := &Device{}
+		dj, _ := json.Marshal(doc)
+		_ = json.Unmarshal(dj, dm)
+		deviceList = append(deviceList, dm)
+	}
+
+	return deviceList
+}
