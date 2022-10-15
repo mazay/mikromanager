825fd1558c96fac104a9ceff010d2eaf5c6be016
Yev Valeyev <z.mazay@gmail.com>
2022-10-15 16:07:26 +0200

    last polled time ago

--- http/utils.go
+++ http/utils.go
@@ -3,12 +3,20 @@ package http
 import (
 	"html/template"
 	"strings"
+	"time"
 )
+
+var funcMap = template.FuncMap{
+	"replace": replace,
+	"timeAgo": timeAgo,
+}
 
 func replace(input, from, to string) string {
 	return strings.Replace(input, from, to, -1)
 }
 
-var funcMap = template.FuncMap{
-	"replace": replace,
+func timeAgo(t time.Time) string {
+	diff := time.Now().Sub(t)
+	out := time.Time{}.Add(diff)
+	return out.Format("15:04:05")
 }
