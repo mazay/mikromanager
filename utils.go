1d77394a30b2c469b9452a09d454bb0fcb1943ad
Yev Valeyev <z.mazay@gmail.com>
2022-10-22 17:10:50 +0200

    utils.go

---
+++ utils.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+func fetchResources(cfg *PollerCFG) error {
+	resource, err := cfg.Client.Run("/system/resource/print")
+	if err != nil {
+		return err
+	}
+	logger.Debugf("fetched resource data for %s", cfg.Client.Address)
+	inrec, _ := json.Marshal(resource[0].Map)
+	return json.Unmarshal(inrec, &cfg.Device)
+}
+
+func fetchIdentity(cfg *PollerCFG) error {
+	identity, err := cfg.Client.Run("/system/identity/print")
+	if err != nil {
+		return err
+	}
+	if len(identity) > 0 {
+		logger.Debugf("identity for %s is %s", cfg.Client.Address, identity[0].Map["name"])
+		cfg.Device.Identity = string(identity[0].Map["name"])
+		return nil
+	}
+	return fmt.Errorf("got an empty identity data")
+}
+
+func writeBackupFile(filename string, data []byte) error {
+	if err := os.MkdirAll(filepath.Dir(filename), 0770); err != nil {
+		logger.Fatal(err)
+		return err
+	}
+
+	f, err := os.Create(filename)
+	if err != nil {
+		logger.Fatal(err)
+		return err
+	}
+	defer f.Close()
+
+	_, err = f.Write(data)
+	if err != nil {
+		logger.Fatal(err)
+		return err
+	}
+	return nil
+}
