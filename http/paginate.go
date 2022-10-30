664dbaec4dc0bbe9000c0884e31b3217a89d3fcb
Yev Valeyev <z.mazay@gmail.com>
2022-10-30 14:51:21 +0100

    getPaginatedUrl

--- http/paginate.go
+++ http/paginate.go
@@ -16,36 +16,30 @@ type Pagination struct {
 	Next     *Page
 }
 
+func getPaginatedUrl(u url.URL, pageNum string) string {
+	values := u.Query()
+	values.Set("page_id", pageNum)
+	u.RawQuery = values.Encode()
+	return u.String()
+}
+
 func (p *Pagination) paginate(u url.URL, current int, total int) {
 	var pages []*Page
 
 	for i := 0; i < total; i++ {
 		pageNum := i + 1
-		values := u.Query()
-		values.Set("page_id", strconv.Itoa(pageNum))
-		u.RawQuery = values.Encode()
-
-		page := &Page{Url: u.String(), Number: pageNum}
+		page := &Page{Url: getPaginatedUrl(u, strconv.Itoa(pageNum)), Number: pageNum}
 		pages = append(pages, page)
 	}
-
 	p.Pages = pages
 
 	if current > 1 {
 		pageNum := current - 1
-		values := u.Query()
-		values.Set("page_id", strconv.Itoa(pageNum))
-		u.RawQuery = values.Encode()
-
-		p.Previous = &Page{Url: u.String(), Number: pageNum}
+		p.Previous = &Page{Url: getPaginatedUrl(u, strconv.Itoa(pageNum)), Number: pageNum}
 	}
 
 	if current < total {
 		pageNum := current + 1
-		values := u.Query()
-		values.Set("page_id", strconv.Itoa(pageNum))
-		u.RawQuery = values.Encode()
-
-		p.Next = &Page{Url: u.String(), Number: pageNum}
+		p.Next = &Page{Url: getPaginatedUrl(u, strconv.Itoa(pageNum)), Number: pageNum}
 	}
 }
