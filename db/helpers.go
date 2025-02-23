a8030d7419399dd9c69e7b339313ada4b393d8de
Yev <z.mazay@gmail.com>
2025-02-23 16:41:47 +0100

    some goDocs

--- db/helpers.go
+++ db/helpers.go
@@ -2,6 +2,8 @@ package db
 
 import "path/filepath"
 
+// openTestDb creates a test database at the given directory and returns a pointer to a DB connection
+// object. This function is used for unit testing purposes.
 func openTestDb(dbDir string) (*DB, error) {
 	dbPath := filepath.Join(dbDir, "test.db")
 	db := &DB{LogLevel: "info"}
@@ -12,6 +14,9 @@ func openTestDb(dbDir string) (*DB, error) {
 	return db, nil
 }
 
+// createTestUser creates and inserts a test user into the provided database.
+// It returns a pointer to the created User object and an error, if any occurs
+// during the user creation process. This function is used for unit testing purposes.
 func createTestUser(db *DB) (*User, error) {
 	user := &User{
 		Username:          "test-user",
